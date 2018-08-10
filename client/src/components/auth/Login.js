232eedff1145f9acd1026fbec80bbdd8043415c9
satya_jha <satyajeetjha06@gmail.com>
2018-08-11 00:44:16 +0530

    changes made to login and register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,64 @@
 import React ,{Component} from 'react';
 
 export default class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            email:'',
+            password:'',
+            errors:{}
+        }
+        this.onChange=this.onChange.bind(this);
+    }
+    onChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value    
+            })
+    }
+    onSubmit=(e)=>{
+        e.preventDefault();
+        const signingin={
+            email:this.state.email,
+            password:this.state.password
+        }
+        console.log(signingin);
+      }
     render(){
         return(
          <div>
-           <h1>Login</h1>
+           <div className="login">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 m-auto">
+          <h1 className="display-4 text-center">Log In</h1>
+          <p className="lead text-center">Sign in to your DevConnector account</p>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <input
+               type="email"
+               value={this.state.email}
+               onChange={this.onChange}
+                className="form-control form-control-lg"
+                 placeholder="Email Address" 
+                 name="email" />
+            </div>
+            <div className="form-group">
+              <input 
+              value={this.state.password}
+              onChange={this.onChange}
+              type="password"
+               className="form-control form-control-lg"
+                placeholder="Password"
+                 name="password" />
+            </div>
+            <input 
+            type="submit" 
+            className="btn btn-info btn-block mt-4" />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
              </div>
 
         );
