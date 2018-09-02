00cd4850e47abd288836c23b294ec6e1147be7fb
satya_jha <satyajeetjha06@gmail.com>
2018-09-02 16:53:32 +0530

    upt profile display is done

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -1,5 +1,5 @@
 import  axios from 'axios';
-import {GET_PROFILE,PROFILE_LOADING,GET_ERRORS,SET_CURRENT_USER, CLEAR_CURRENT_PROFILE} from './types';
+import {GET_PROFILE,GET_PROFILES,PROFILE_LOADING,GET_ERRORS,SET_CURRENT_USER, CLEAR_CURRENT_PROFILE} from './types';
 // get current profile 
 export const getCurrentProfile =()=>dispatch=>{
   dispatch(setProfileLoading());
@@ -102,6 +102,42 @@ export const deleteExperience = id => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
+};
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
       })
     );
 };
